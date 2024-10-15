4a3eebf650d7908ddddcec57aa3feb36ae83a0bf
alwaw <124403241+alwaw@users.noreply.github.com>
2024-10-15 21:48:18 +0200

    hotfix

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Navbar />
       <div className={styles.wrapper_two_sides}>
         <ImageHolder
-          src={"DorS/photos/with-laptop.jpg"}
+          src={"/DorS/photos/with-laptop.jpg"}
           alt="assistant's big picture"
         />
         <Header>
